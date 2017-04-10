0de06e58196acf6432d9465cd433e7186bc2ccf2
Viktor Willmann <viktorwillmann@viktors-mini.fritz.box>
2017-04-10 20:54:19 +0200

    Initial Commit

---
+++ chatOffset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chatOffset
+//
+//  Created by Viktor Willmann on 10.04.17.
+//  Copyright © 2017 Viktor Willmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
